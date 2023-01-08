14bdbdbba2c850f5416e8680bc87706ffc959564
lika lika <lika2882@mail.ru>
2023-01-09 01:30:08 +0300

    fist commit

--- task_final/solution.js
+++ task_final/solution.js
@@ -35,19 +35,35 @@
 
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
-
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i ++) {
+    data.goods.push({title:goods[i].title, count:goods[i].count});
+    //data.goods.push(goods[i].title);
+    
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж" + ", кв. " + address.flat;
+    data.order.sum = sum;
+    data.client = name + " " + phone;
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    let jsonData = JSON.stringify({data});
     return jsonData;
 }
+
+    //let data = {order: {address, sum}, client: {name, phone}, goods:[]};
+    //let data = {data: {client: "Иван +7(987)65-43-210", order : {address: "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53", sum: 900},
+//goods: [{title: "Пицца", count: 2}] };
+//data.client = "Иван +7(987)65-43-210";
+
+
+    
+    
+
+
+    //for (let i = 0; i <= countOfGoods; i += 1) {
+       // data.goods(goods[i].title);
+    
+
+    
+    //data.order.sum = name + phoneaddress + goods + sum;
+    //data.order.sum = sum;
+   // data.client = 'Иван';
+    
